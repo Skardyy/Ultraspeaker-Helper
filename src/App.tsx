2a88212b89c18eb609b00749a19dae4ec8231777
skardy <meronb1312@gmail.com>
2024-12-06 18:46:53 +0200

    just moved things, not important

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import { invoke } from "@tauri-apps/api/core";
-import { Box, Button, Checkbox, Input, MantineProvider, Select, Text, createTheme } from '@mantine/core';
+import { Box, Button, Checkbox, Input, Select, Text } from '@mantine/core';
 import { useEffect, useState } from "react";
 import ListItem from "./components/ListItem";
 import { register, unregisterAll } from '@tauri-apps/plugin-global-shortcut';
 import { getCurrentWindow, LogicalSize } from '@tauri-apps/api/window';
-
-const theme = createTheme({
-  fontFamily: 'sans-serif',
-  primaryColor: 'blue',
-});
 
 interface Block {
   zone_holder: string
@@ -84,36 +79,34 @@ function App() {
   }
 
   return (
-    <MantineProvider theme={theme} defaultColorScheme="dark">
-      <Box style={{ display: 'flex', flexDirection: 'column', padding: 12, height: '100vh', overflow: 'hidden' }}>
-        <Text size="md" color={zone === block?.zone_holder ? 'red' : ''} style={{ textAlign: 'center' }}>
-          {block?.zone_holder ?? 'NAN'}
-        </Text>
-        <Box style={{ justifyContent: 'center', alignItems: 'center', gap: 12, marginTop: 6, display: 'flex', flexDirection: 'column' }}>
-          {block?.calls.map((val, i) => (
-            <ListItem me={taunts} warning={val.warning} taunter={val.taunter} key={i} />
-          ))}
-        </Box>
-
-        {!started &&
-          <Box style={{ display: 'flex', flexDirection: 'column', gap: 12, height: '100%' }}>
-            <Box style={{ display: 'flex', flexDirection: 'row', gap: 6, justifyContent: 'space-between', marginTop: 'auto' }}>
-              <Checkbox size="sm" label="SC" onChange={val => handleZoneChange('SC', val.target.checked)} />
-              <Checkbox size="sm" label="LR" onChange={val => handleZoneChange('LR', val.target.checked)} />
-              <Checkbox size="sm" label="LOO" onChange={val => handleZoneChange('LOO', val.target.checked)} />
-              <Checkbox size="sm" label="AP" onChange={val => handleZoneChange('AP', val.target.checked)} />
-            </Box>
-            <Select placeholder="Zone" data={['SC', 'LR', 'LOO', 'AP']} onChange={(val) => setZone(val ?? '')} size="xs" />
-            <Box style={{ display: 'flex', width: '100%', justifyContent: 'space-around', gap: 12 }}>
-              <Input maxLength={1} onChange={val => handleShortcutChange(val.target.value)} value={shortcut} placeholder='shortcut' size="xs" style={{ width: '100%' }} />
-              <Button size="xs" style={{ width: '100%' }} variant="light" onClick={start} >
-                Start
-              </Button>
-            </Box>
-          </Box>
-        }
+    <Box style={{ display: 'flex', flexDirection: 'column', padding: 12, height: '100vh', overflow: 'hidden' }}>
+      <Text size="md" color={zone === block?.zone_holder ? 'red' : ''} style={{ textAlign: 'center' }}>
+        {block?.zone_holder ?? 'NAN'}
+      </Text>
+      <Box style={{ justifyContent: 'center', alignItems: 'center', gap: 12, marginTop: 6, display: 'flex', flexDirection: 'column' }}>
+        {block?.calls.map((val, i) => (
+          <ListItem me={taunts} warning={val.warning} taunter={val.taunter} key={i} />
+        ))}
       </Box>
-    </MantineProvider >
+
+      {!started &&
+        <Box style={{ display: 'flex', flexDirection: 'column', gap: 12, height: '100%' }}>
+          <Box style={{ display: 'flex', flexDirection: 'row', gap: 6, justifyContent: 'space-between', marginTop: 'auto' }}>
+            <Checkbox size="sm" label="SC" onChange={val => handleZoneChange('SC', val.target.checked)} />
+            <Checkbox size="sm" label="LR" onChange={val => handleZoneChange('LR', val.target.checked)} />
+            <Checkbox size="sm" label="LOO" onChange={val => handleZoneChange('LOO', val.target.checked)} />
+            <Checkbox size="sm" label="AP" onChange={val => handleZoneChange('AP', val.target.checked)} />
+          </Box>
+          <Select placeholder="Zone" data={['SC', 'LR', 'LOO', 'AP']} onChange={(val) => setZone(val ?? '')} size="xs" />
+          <Box style={{ display: 'flex', width: '100%', justifyContent: 'space-around', gap: 12 }}>
+            <Input maxLength={1} onChange={val => handleShortcutChange(val.target.value)} value={shortcut} placeholder='shortcut' size="xs" style={{ width: '100%' }} />
+            <Button size="xs" style={{ width: '100%' }} variant="light" onClick={start} >
+              Start
+            </Button>
+          </Box>
+        </Box>
+      }
+    </Box>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import '@mantine/core/styles.css';
+import { createTheme, MantineProvider, ColorSchemeScript } from "@mantine/core";
+
+const theme = createTheme({
+  fontFamily: 'sans-serif',
+  primaryColor: 'blue',
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ColorSchemeScript />
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
